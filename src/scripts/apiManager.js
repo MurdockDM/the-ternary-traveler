0248494f60fdaa116805264b2425ef2e910c0ebb
Dustin Murdock <dmurdock2@gmail.com>
2020-02-13 11:30:56 -0600

    added save function for initial entries without review necessary

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,2 +1,25 @@
 
 
+const baseURL = "http://localhost:8088"
+
+const apiManager = {
+    getInterestEntries () {
+        return fetch(`${baseURL}/interests`)
+            .then(response => response.json())
+    },
+    addInterestEntry (interestEntry) {
+        return fetch(`${baseURL}/interests`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(interestEntry)
+        })
+    }
+
+
+
+
+}
+
+export default apiManager
--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,9 +1,43 @@
+import apiManager from "./apiManager.js";
+
+const poiName = document.getElementById("interestName");
+const poiDescription = document.getElementById("interestDescription");
+const poiCost = document.getElementById("interestCost");
+const poiReview = document.getElementById("interestReview");
+const poiCountry = document.getElementById("countryChoice");
+
+
+const clearFormFields = () => {
+    poiName.value = ""
+    poiDescription.value = ""
+    poiCost.value = ""
+    poiReview.value = ""
+    poiCountry.value = ""
+}
+
 
 const eventListenerHandler = {
     saveInterestAddEventListener () {
-        const saveButton = document.getElementById("interestSaveBtn")
+        const saveButton = document.getElementById("interestSaveBtn");
 
-        
+        saveButton.addEventListener("click", () => {
+            const pointOfInterestFactory = () => {
+                const parsedCountry = parseInt(poiCountry.value)
+                return {
+                    placeId: parsedCountry,
+                    name: poiName.value,
+                    description: poiDescription.value,
+                    cost: poiCost.value,
+                    review: poiReview.value
+                  }
+            }
+
+            if (poiCountry == "" || poiName == "" || poiDescription == "" || poiCost == "") {
+                alert("All fields except review are required.")
+            } else {
+                apiManager.addInterestEntry(pointOfInterestFactory())
+            }
+        })
 
     }
 
